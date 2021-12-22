cce6e36aa3a44df57be90baf58b4863a799544a8
Alireza <alikro2000@gmail.com>
2021-12-22 19:04:02 +0330

    ➕Implemented a basic Indexer.

--- blooper_indexer/bin/blooper_indexer.dart
+++ blooper_indexer/bin/blooper_indexer.dart
@@ -1,3 +1,14 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+import 'indexer.dart';
+
+void main(List<String> arguments) async {
+  String dbPath = './hive_database';
+  // File file = File(dbPath);
+  // if (!file.existsSync()) {
+  //   file.createSync();
+  // }
+  Indexer indexer = Indexer(dbPath);
+  // indexer.index(1, 'سلام یا سلامتی و تندرستی', 't');
+  print(await indexer.getWordIndex('سلام'));
 }
---
+++ blooper_indexer/bin/indexer.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+import 'package:crypto/crypto.dart' as crypto;
+
+class Indexer {
+  Indexer(String dataBasePath) {
+    Hive.init(dataBasePath);
+  }
+
+  Future<void> index(int docID, String content, String type) async {
+    List<String> processedWords = [];
+    for (String word in content.split(' ').map((e) => e.trim())) {
+      if (processedWords.contains(word)) {
+        continue;
+      }
+      String wordHash = crypto.md5.convert(utf8.encode(word)).toString();
+      (await Hive.openBox(wordHash)).put(docID, {
+        'freq': countFrequency(content, word),
+        'type': type,
+      });
+      processedWords.add(word);
+    }
+  }
+
+  Future<List> getWordIndex(String word) async {
+    List result = [];
+    String wordHash = crypto.md5.convert(utf8.encode(word)).toString();
+    var box = await Hive.openBox(wordHash);
+    for (int docID in box.keys) {
+      var row = box.get(docID);
+      result.add({
+        'docID': docID,
+        'freq': row['freq'],
+        'type': row['type'],
+      });
+    }
+    return result;
+  }
+
+  int countFrequency(String haystack, String needle) {
+    int count = 0;
+    int i = -1;
+    while ((i = haystack.indexOf(needle, i + 1)) > 0) {
+      ++count;
+    }
+    return count;
+  }
+}
