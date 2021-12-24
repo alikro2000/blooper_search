82043549c431e89b3ad2186d8e3e1c0e68f32df5
Alireza <alikro2000@gmail.com>
2021-12-25 00:55:04 +0330

    ➕Added a search screen

--- blooper_client/lib/main.dart
+++ blooper_client/lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:blooper_client/core/get_it.dart';
-import 'package:blooper_client/domain/repository/query_repository.dart';
-import 'package:blooper_client/domain/usecase/get_query_result.dart';
+import 'package:blooper_client/presentation/screens/search_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 
 void main() async {
   GetItInitializer.initGetIt();
-  (await GetIt.instance<GetQueryResult>()
-          .call('دانشگاه NOT تهران NOT یزد NOT یا NOT و'))
-      .fold(
-    (l) => print(l),
-    (r) => print(r.query),
-  );
 
   runApp(const MyApp());
 }
@@ -27,11 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Center(
-          child: Text('Testing... Check your console for info.'),
-        ),
-      ),
+      home: const SearchScreen(),
     );
   }
 }
