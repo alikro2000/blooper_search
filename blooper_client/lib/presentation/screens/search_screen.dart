c21f62562aad9bdff5b94340edb380c184ed6de4
Alireza <alikro2000@gmail.com>
2021-12-25 02:32:44 +0330

    🔨Added a simple UI layout

--- blooper_client/lib/presentation/screens/search_screen.dart
+++ blooper_client/lib/presentation/screens/search_screen.dart
@@ -8,21 +8,85 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          children: const [
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             //Header and Search bar
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              child: Row(
+                children: [
+                  //GitHub clickable logo
+                  Placeholder(
+                    fallbackWidth: 50,
+                    fallbackHeight: 50,
+                    color: Colors.grey.shade800,
+                  ),
+                  const SizedBox(width: 20),
+                  //Blooper Search Icon
+                  Placeholder(
+                    fallbackWidth: 250,
+                    fallbackHeight: 50,
+                    color: Colors.grey.shade800,
+                  ),
+                  const SizedBox(width: 20),
+                  //Serach Bar
+                  Expanded(
+                    child: TextField(
+                      style: const TextStyle(
+                        fontFamily: 'IRANSans',
+                        fontSize: 32,
+                      ),
+                      textDirection: TextDirection.rtl,
+                      decoration: InputDecoration(
+                        hintText: 'جستجوی خود را وارد کنید...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(45),
+                          borderSide: BorderSide(
+                            width: 3,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        prefixIcon: Container(
+                          margin: const EdgeInsets.only(left: 20),
+                          child: IconButton(
+                            hoverColor: Colors.grey.withOpacity(0.15),
+                            onPressed: () => print('Submit the data!'),
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.grey.shade700,
+                              size: 24,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onSubmitted: (value) => print('Submitted: ' + value),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
             //Results text view
-            Text(
-              'Found 2500 results in 3 seconds for the query: Hello World!',
-              style: TextStyle(
-                fontFamily: 'Segoe_UI',
-                fontSize: 18,
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(left: 200),
+              child: const Text(
+                'Found 2500 results in 3 seconds for the query: Hello World!',
+                style: TextStyle(
+                  fontFamily: 'Segoe_UI',
+                  fontSize: 18,
+                ),
               ),
             ),
 
             //Result Cards
+            const SingleChildScrollView(
+              child: Text(
+                'Search Results:\n...',
+              ),
+            ),
 
-            //Pagination
+            //TODO: Pagination
           ],
         ),
       ),
